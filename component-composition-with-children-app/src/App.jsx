8a1867ffd49aa9e8e238b5a87f26875d9c5d4226
Edmon the Programmer <41594880+edmontheprogrammer@users.noreply.github.com>
2023-04-10 06:12:37 +0200

    types of children app

--- component-composition-with-children-app/src/App.jsx
+++ component-composition-with-children-app/src/App.jsx
@@ -3,31 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Button = ({children, backgroundColor}) => {
+  return <button style={{ background }}>{children}</button>
+}
+
+
+const Alert = ({ children }) => {
+  return (
+    <>
+      <div className='Overlay'  />
+      <div className='Alert'/>
+    </>
+  );
+};
+
+const DeleteButton = () => {
+  return <Button backgroundColor="red">Delete</Button>; 
+};
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <header>Little Lemon Restaurant</header> 
+        <Alert>
+              <h4>Delete Account</h4>
+              <p>
+                  Are you sure you want to proceed? You will miss all your delicious recipes!
+              </p>
+              <DeleteButton />
+        </Alert>
     </div>
   )
 }
---
+++ types-of-children-app/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Alert } from 'bootstrap'
+
+function App() {
+  return (
+    // <div className="App">
+    //       <div>     Little Lemon 1    </div>
+    //       <div>
+
+
+    //         Little Lemon 2
+    //       </div>
+    //       <div>
+    //         Little 
+    //         Lemon 3
+    //       </div>
+    //       {/* <Alert>
+    //           <Title />
+    //           <Body />
+    //       </Alert> */}
+    //       {/* <Alert>
+    //           <div>Are you sure?</div>
+    //           <Body />
+    //       </Alert> */}
+
+    // </div>
+
+    <>
+      <li>Pizza margarita</li>
+      <li>Pizza diavola</li>
+    </>
+  )
+}
+
+export default App
